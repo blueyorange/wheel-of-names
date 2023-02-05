8c95813b0f6113dc62cef59c4ae731a1e6ec21a0
Russell Johnson <blueyorange@gmail.com>
2023-02-05 15:30:23 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import courses from "./names.js";
+
+const dom = {
+  app: document.querySelector("#app"),
+  courseSelect: app.querySelector("#courseSelector"),
+  namesUl: app.querySelector(".names"),
+};
+
+const courseSelectEl = courses.forEach((course) => {
+  const newOptionElement = document.createElement("option");
+  newOptionElement.value = course.name;
+  newOptionElement.innerText = course.name;
+  dom.courseSelect.appendChild(newOptionElement);
+});
+
+function addName(nameStr) {
+  const list = document.querySelector(".names");
+  const item = list.querySelector("template").content.cloneNode(true);
+  item.querySelector("label").innerText = nameStr;
+  list.appendChild(item);
+}
+
+const selectedCourse = courses[0];
+
+function populateList(names) {}
