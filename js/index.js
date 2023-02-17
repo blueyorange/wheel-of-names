2c53b68d68fa4cb76e52987b5ce67fa21fa8dcf0
Russell Johnson <blueyorange@gmail.com>
2023-02-17 22:41:56 +0000

    fixed bug hadn't updated method names properly

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 import Names from "./names-model.js";
 
-const model = Names({
-  listEl: document.querySelector('.names'),
+const names = Names({
+  listEl: document.querySelector(".names"),
   currAngle: 0,
   onAdd(name) {
-    const newNameEl = document.createElement('li');
-    newNameEl.id=name;
-    newNameEl.innerText=name;
+    const newNameEl = document.createElement("li");
+    newNameEl.id = name;
+    newNameEl.innerText = name;
     // newNameEl.addEventListener('click', () => model.deleteName(name));
     this.listEl.appendChild(newNameEl);
   },
@@ -16,50 +16,58 @@ const model = Names({
   },
 
   onClearNames() {
-    [...this.listEl.children].forEach(el => el.remove());
+    [...this.listEl.children].forEach((el) => el.remove());
   },
 
   onCountChange(count) {
     [...this.listEl.children].forEach((nameElement, n) => {
-      let x = 50*(1/Math.tan(3.14/count)+1);
-      nameElement.style.setProperty('clip-path', `polygon(50% 50%, ${x}% 0%, 100% 0%, 100% 100%, ${x}% 100%)`);
-      nameElement.style.setProperty('transform', `rotate(${n/count}turn)`);
-      let hue = Math.floor(360*n/6);
-      nameElement.style.setProperty('background-color', `hsl(${hue}, 100%, 20%)`);
-    })
+      let x = 50 * (1 / Math.tan(3.14 / count) + 1);
+      nameElement.style.setProperty(
+        "clip-path",
+        `polygon(50% 50%, ${x}% 0%, 100% 0%, 100% 100%, ${x}% 100%)`
+      );
+      nameElement.style.setProperty("transform", `rotate(${n / count}turn)`);
+      let hue = Math.floor(360 / n);
+      nameElement.style.setProperty(
+        "background-color",
+        `hsl(${hue}, 100%, 50%)`
+      );
+    });
   },
 
   onSpinStart(index) {
-    [...this.listEl.children].forEach(el => el.classList.remove('selected'))
+    [...this.listEl.children].forEach((el) => el.classList.remove("selected"));
     const minimumTurns = 5;
     const length = [...this.listEl.children].length;
-    const angleOffset = Math.random()/(2*length)-Math.random()/(4*length);
-    const endAngle = angleOffset-index/length+minimumTurns;
-    console.log('index', index);
+    const angleOffset =
+      Math.random() / (2 * length) - Math.random() / (4 * length);
+    const endAngle = angleOffset - index / length + minimumTurns;
+    console.log("index", index);
     console.log(`currAngle: ${this.currAngle}; endAngle: ${endAngle}`);
-    document.querySelector(':root').style.setProperty('--start-angle', `${this.currAngle}turn`);
-    document.querySelector(':root').style.setProperty('--end-angle', `${endAngle}turn`);
+    document
+      .querySelector(":root")
+      .style.setProperty("--start-angle", `${this.currAngle}turn`);
+    document
+      .querySelector(":root")
+      .style.setProperty("--end-angle", `${endAngle}turn`);
     this.currAngle = endAngle % 1;
-    this.listEl.classList.remove('spin-wheel');
+    this.listEl.classList.remove("spin-wheel");
     setTimeout(() => {
-      this.listEl.classList.add('spin-wheel');
-    },1)
+      this.listEl.classList.add("spin-wheel");
+    }, 1);
   },
-  
+
   onSpinEnd(name) {
-    this.listEl.querySelector(`#${name}`).classList.add('selected');
+    this.listEl.querySelector(`#${name}`).classList.add("selected");
     console.log(name);
-  }
+  },
 });
 
 window.addEventListener("submit", (e) => e.preventDefault(), { capture: true });
-document.forms.names.elements.clear.addEventListener('click', model.clearNames);
-document.forms.names.addEventListener(
-  "submit",
-  e => {
-    model.addName(e.target.elements.add.value);
-    e.target.elements.add.value = "";
-  }
-);
-document.querySelector('.names').addEventListener('click', model.select);
-document.querySelector('.names').addEventListener('animationend', model.reveal);
+document.forms.names.elements.clear.addEventListener("click", names.clear);
+document.forms.names.addEventListener("submit", (e) => {
+  names.add(e.target.elements.add.value);
+  e.target.elements.add.value = "";
+});
+document.querySelector(".names").addEventListener("click", names.select);
+document.querySelector(".names").addEventListener("animationend", names.reveal);
