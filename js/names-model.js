0974fee94183a4c29bfda19d7a95b50e8b2a31c0
Russell Johnson <blueyorange@gmail.com>
2023-03-24 08:28:54 +0000

    model changed to class instance of set

--- js/names-model.js
+++ js/names-model.js
@@ -1,45 +1,28 @@
-export default function Names(options = {}) {
-  const obj = {};
-  const { storageKey } = options || "names";
-  let names = new Set();
-  let selected = null;
-  const asJson = localStorage.getItem(storageKey);
-  if (asJson) {
-    names = new Set(JSON.parse(asJson));
-    names.forEach((value, key) => {
-      client.onAdd(key, value);
-    });
+export default class Names extends Set {
+  constructor(cb) {
+    const storageKey = "names";
+    const asJson = localStorage.getItem(storageKey);
+    if (asJson) {
+      super(JSON.parse(asJson));
+    } else {
+      super();
+    }
+    this.storageKey = storageKey;
+    this.cb = cb;
   }
 
-  return Object.freeze({
-    get() {
-      return names;
-    },
+  save() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this));
+    this.cb();
+  }
 
-    save() {
-      console.log(JSON.stringify([...names]));
-      localStorage.setItem(storageKey, JSON.stringify([...names]));
-      obj.dispatchEvent(new CustomEvent("save"));
-    },
+  add(name) {
+    super.add(name);
+    this.save();
+  }
 
-    add() {
-      if (!names.has(name)) {
-        names.add(name);
-        client.onAdd(name);
-        save();
-      }
-    },
-
-    remove(name) {
-      names.delete(name);
-      client.onDelete(name);
-      save();
-    },
-
-    clear() {
-      client.onClearNames([...names]);
-      names = new Set();
-      save();
-    },
-  });
+  clear() {
+    super.clear();
+    this.save();
+  }
 }
